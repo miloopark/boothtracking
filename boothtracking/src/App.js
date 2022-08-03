11a08737ea6565cd62cab3b20d799f2d5fe9604c
Elaine Leiyoung <92842749+elaineleiyoung@users.noreply.github.com>
2022-08-02 17:07:18 -0700

    layout

--- boothtracking/src/App.js
+++ boothtracking/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import booth from './booth.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={booth} className="booth1" />
+      <img src={booth} className="booth2" />
+      <img src={booth} className="booth3" />
+      <img src={booth} className="booth4" />
+      <img src={booth} className="booth5" />
+      <img src={booth} className="booth6" />
+      <img src={booth} className="booth7" />
+      <img src={booth} className="booth8" />
+      <img src={booth} className="booth9" />
     </div>
   );
 }
